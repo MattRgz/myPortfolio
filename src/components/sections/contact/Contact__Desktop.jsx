7f109eb653c502ef433e0ef92af94689f655b5e7
Matias Reyes <mreyesg10@correo.uss.cl>
2022-11-13 15:39:54 -0300

    Form Added.

--- src/components/sections/contact/Contact__Desktop.jsx
+++ src/components/sections/contact/Contact__Desktop.jsx
@@ -1,10 +1,22 @@
-import React from 'react';
+import React,{useState} from 'react';
 import ContactStyles__Desktop from "./ContactStyles__Desktop.scss";
 import githubContact from "../../../assets/images/githubContact.png";
 import linkedInContact from "../../../assets/images/linkedInContact.png";
 import mailContact from "../../../assets/images/mailContact.png";
+import {useForm} from "react-hook-form";
+
 
 const ContactDesktop = () => {
+    const [formActive, setFormActive] = useState(false);
+    const activatingForm = () =>{
+        setFormActive(!formActive)
+    }
+    const {register, handleSubmit,formState:{errors}} = useForm();
+
+    const onSubmit = (data) =>{
+        console.log(data);
+    }
+
     return (
         <div className='contactContainer'>
             <h1 className='letsTalkText'>
@@ -18,7 +30,7 @@ const ContactDesktop = () => {
                             <img src={githubContact} alt="Github" />    
                         </a>
                     </li>
-                    <li>
+                    <li onClick={(e)=>activatingForm()}>
                         <img src={mailContact} alt="eMail" />
                     </li>
                     <li>
@@ -27,6 +39,46 @@ const ContactDesktop = () => {
                         </a>
                     </li>
                 </ul>
+            </div>
+            <div>
+                <div className={formActive?'showForm':'hideForm'}>
+                    <form onSubmit={handleSubmit(onSubmit)}>
+                            <div>
+                                <label>Name</label>
+                                <input type="text" {...register('userName',
+                                {required:true,
+                                maxLength:20
+                                })} />
+                                <div>
+                                    {errors.userName?.type === 'required' && <p>El campo es requerido.</p>}
+                                    {errors.userName?.type === 'maxLength' && <p>El campo debe tener menos de 20 caracteres.</p>}
+                                </div>
+                            </div>
+                            <div>
+                                <label>Mail</label>
+                                <input type="email" {...register('userMail',
+                                {required:true,
+                                    pattern: /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/
+                                })} />
+                                <div>
+                                    {errors.userMail?.type === 'required' && <p>El campo es requerido.</p>}
+                                    {errors.userMail?.type === 'pattern' && <p>Por favor ingrese un email valido!</p>}
+                                </div>
+                            </div>
+                            <div>
+                                <label>Message</label>
+                                <textarea {...register('message',
+                                {required:true,
+                                maxLength:100
+                                })} />
+                                <div>
+                                    {errors.message?.type === 'required' && <p>El campo es requerido.</p>}
+                                    {errors.message?.type === 'maxLength' && <p> El campo debe tener menos de 100 caracteres.</p>}
+                                </div>
+                            </div>
+                            <input type="submit" value="Send" />
+                    </form>
+                </div>
             </div>
         </div>
     );
