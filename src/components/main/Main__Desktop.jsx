4af344f3692471c5ea5d989ebfe34a44e1138422
Matias Reyes <mreyesg10@correo.uss.cl>
2022-11-14 02:56:42 -0300

    Projects Updated!

--- src/components/main/Main__Desktop.jsx
+++ src/components/main/Main__Desktop.jsx
@@ -3,7 +3,9 @@ import MainStyles__Desktop from "./MainStyles__Desktop.scss";
 import { useInView  } from "react-intersection-observer";
 import ejemplo from "../../assets/images/Ejemplo.png";
 import taskifyPic from "../../assets/images/taskifyPic.png";
-import githubMinIcon from "../../assets/images/githubMinIcon.png"
+import githubMinIcon from "../../assets/images/githubMinIcon.png";
+import GestorNPic from "../../assets/images/GestorNPic.png";
+import myPortfolioPic from "../../assets/images/myPortfolioPic.png";
 
 const MainDesktop = () => {
     const {ref: firstProyect, inView: leftEntry} = useInView();
@@ -12,10 +14,10 @@ const MainDesktop = () => {
     return (
         <div className='proyectContainer'>
             <div ref={firstProyect} className={`${"proyectAtLeft"} ${leftEntry?"entryFromLeft":""}` }>
-                <div style={{color:"black", textAlign:'justify',width:'30%'}}>
-                    <p style={{color:"black"}}>Taskify is a "To Do List" coded as a project for my bootcamp. You can create a profile and manage the list with products previusly created.</p>
+                <div  style={{color:"black", textAlign:'justify',width:'30%'}}>
+                    <p style={{color:"black"}}>My first project was called GestorN, It was born as a easily customizable management web for small business. It's important to say that it was my first BootCamp project, and I's mainly aimed at backend development. </p>
                     <div className='codeLinkButtonBox'>
-                        <a target="_blank"  rel="noreferrer" href="https://github.com/MattRgz/Taskify" style={{textDecoration:'none'}}>
+                        <a target="_blank"  rel="noreferrer" href="https://github.com/MattRgz/ECRProject/tree/master/proyectoIndividualECR" style={{textDecoration:'none'}}>
                             <button className='codeLinkButton' style={{display:"flex",alignItems:'center',justifyContent:'center'}}> 
                                 <img src={githubMinIcon} alt="GHIcon" style={{width:'20px',borderRadius:'25px'}}/>
                                 <div style={{display:'flex',justifyContent:'end',alignContent:'center',width:'65%'}}>
@@ -27,14 +29,14 @@ const MainDesktop = () => {
                         </a>
                     </div>
                 </div>
-                <img src={taskifyPic} alt="ACA VA UNA IMAGEN 1" />
+                <img src={GestorNPic} alt="GestorN" />
             </div>
             <div ref={secondProyect}  className={`${"proyectAtRight"} ${rightEntry?"entryFromRight":""}` }>
-                <img src={taskifyPic} alt="ACA VA UNA IMAGEN 2" />
+                <img src={taskifyPic} alt="Taskify" />
                 <div style={{color:"black", textAlign:'justify',width:'30%'}}>
-                    <p style={{color:"black"}}>Proyecto 2</p>
+                    <p style={{color:"black"}}>Taskify is a "To Do List" coded as a second project for my BootCamp graduation. You can create a profile and manage a list with products previusly created by the page's administrator. For this project I used design tools that can be reviewed on the documentation. </p>
                     <div className='codeLinkButtonBox'>
-                        <a target="_blank"  rel="noreferrer" href="#" style={{textDecoration:'none'}}>
+                        <a target="_blank"  rel="noreferrer" href="https://github.com/MattRgz/Taskify" style={{textDecoration:'none'}}>
                             <button className='codeLinkButton' style={{display:"flex",alignItems:'center',justifyContent:'center'}}> 
                                 <img src={githubMinIcon} alt="GHIcon" style={{width:'20px',borderRadius:'25px'}}/>
                                 <div style={{display:'flex',justifyContent:'end',alignContent:'center',width:'65%'}}>
@@ -49,9 +51,9 @@ const MainDesktop = () => {
             </div>
             <div ref={thirdProyect}  className={`${"proyectAtLeft"} ${secondLeftEntry?"entryFromLeft":""}` }>
                 <div style={{color:"black", textAlign:'justify',width:'30%'}}>  
-                    <p style={{color:"black"}}>Proyecto 3</p>
+                    <p style={{color:"black"}}>Last but not least, my portfolio is my third project. I tried to give it more importance to the frontend development at the same time that I learned how to design and implement 3d stuffs. Honestly, a great achievement for me.</p>
                     <div className='codeLinkButtonBox'>
-                        <a target="_blank"  rel="noreferrer" href="#" style={{textDecoration:'none'}}>
+                        <a target="_blank"  rel="noreferrer" href="https://github.com/MattRgz/myPortfolio" style={{textDecoration:'none'}}>
                             <button className='codeLinkButton' style={{display:"flex",alignItems:'center',justifyContent:'center'}}> 
                                 <img src={githubMinIcon} alt="GHIcon" style={{width:'20px',borderRadius:'25px'}}/>
                                 <div style={{display:'flex',justifyContent:'end',alignContent:'center',width:'65%'}}>
@@ -63,7 +65,7 @@ const MainDesktop = () => {
                         </a>
                     </div>
                 </div>
-                <img src={taskifyPic} alt="ACA VA UNA IMAGEN 3" />
+                <img src={myPortfolioPic} alt="My potfolio" />
             </div>
         </div>
     );
