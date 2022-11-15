f4b28336b3431035bdcb0fb01dba549495baa998
Matias Reyes <mreyesg10@correo.uss.cl>
2022-11-14 22:27:19 -0300

    Projects grammatic.

--- src/components/main/Main__Desktop.jsx
+++ src/components/main/Main__Desktop.jsx
@@ -15,7 +15,7 @@ const MainDesktop = () => {
         <div className='proyectContainer'>
             <div ref={firstProyect} className={`${"proyectAtLeft"} ${leftEntry?"entryFromLeft":""}` }>
                 <div  style={{color:"black", textAlign:'justify',width:'30%'}}>
-                    <p style={{color:"black"}}>My first project was called GestorN, It was born as a easily customizable management web for small business. It's important to say that it was my first BootCamp project, and I's mainly aimed at backend development. </p>
+                    <p style={{color:"black"}}>My first project was called GestorN, It was born as an easy customizable management web for small business. It's important to say that it was my first BootCamp project, and I's mainly aimed at backend development. </p>
                     <div className='codeLinkButtonBox'>
                         <a target="_blank"  rel="noreferrer" href="https://github.com/MattRgz/ECRProject/tree/master/proyectoIndividualECR" style={{textDecoration:'none'}}>
                             <button className='codeLinkButton' style={{display:"flex",alignItems:'center',justifyContent:'center'}}> 
@@ -34,7 +34,7 @@ const MainDesktop = () => {
             <div ref={secondProyect}  className={`${"proyectAtRight"} ${rightEntry?"entryFromRight":""}` }>
                 <img src={taskifyPic} alt="Taskify" />
                 <div style={{color:"black", textAlign:'justify',width:'30%'}}>
-                    <p style={{color:"black"}}>Taskify is a "To Do List" coded as a second project for my BootCamp graduation. You can create a profile and manage a list with products previusly created by the page's administrator. For this project I used design tools that can be reviewed on the documentation. </p>
+                    <p style={{color:"black"}}>Taskify is a "To Do List" coded as a second project for my BootCamp graduation. You can create a profile and manage a list with products previously created by the page's administrator. For this project I used design tools that can be reviewed on the documentation. </p>
                     <div className='codeLinkButtonBox'>
                         <a target="_blank"  rel="noreferrer" href="https://github.com/MattRgz/Taskify" style={{textDecoration:'none'}}>
                             <button className='codeLinkButton' style={{display:"flex",alignItems:'center',justifyContent:'center'}}> 
@@ -51,7 +51,7 @@ const MainDesktop = () => {
             </div>
             <div ref={thirdProyect}  className={`${"proyectAtLeft"} ${secondLeftEntry?"entryFromLeft":""}` }>
                 <div style={{color:"black", textAlign:'justify',width:'30%'}}>  
-                    <p style={{color:"black"}}>Last but not least, my portfolio is my third project. I tried to give it more importance to the frontend development at the same time that I learned how to design and implement 3d stuffs. Honestly, a great achievement for me.</p>
+                    <p style={{color:"black"}}>Last but not least, my portfolio is my third project. I tried to give more importance to the frontend development at the same time that I learned how to design and implement 3D objects. Honestly, a great achievement for me.</p>
                     <div className='codeLinkButtonBox'>
                         <a target="_blank"  rel="noreferrer" href="https://github.com/MattRgz/myPortfolio" style={{textDecoration:'none'}}>
                             <button className='codeLinkButton' style={{display:"flex",alignItems:'center',justifyContent:'center'}}> 
